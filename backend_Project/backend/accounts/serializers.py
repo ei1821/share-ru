dbd92bf6c7e43eff0a9f49e56b06052ddc75fcc4
ei1821 <47051065+ei1821@users.noreply.github.com>
2023-12-29 15:52:42 +0900

    無駄な部分を削除

--- backend_Project/backend/accounts/serializers.py
+++ backend_Project/backend/accounts/serializers.py
@@ -2,23 +2,7 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer #追�
 
 #トークンを発行するためのクラス
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-
     @classmethod
     def get_token(cls, user):
         token = super(MyTokenObtainPairSerializer, cls).get_token(user)
-
-        # Add custom claims
         return token
-    
-from rest_framework import serializers
-from .models import User
-
-class RegisterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        field = ('user_id', 'name', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
-
-        def create(self, validated_data):
-            user = User.objects.create_user(**validated_data)
-            return user
--- backend_Project/backend/accounts/views.py
+++ backend_Project/backend/accounts/views.py
@@ -11,27 +11,3 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 #追加
 class ObtainTokenPairWithColorView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
-
-class RegisterView(APIView):
-    @staticmethod
-    def post(request, *args, **kwargs):
-        print(request.data)
-        serializer = RegisterSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            # パスワードと確認パスワードが一致しない場合
-            if serializer.validated_data['password'] != request.data['password_confirmation']:
-                return Response({'error': 2}, status=HTTP_400_BAD_REQUEST)
-
-            # UserIDがすでに使われていた場合
-            if User.objects.filter(user_id=serializer.validated_data['user_id']).exists():
-                return Response({'error': 3}, status=HTTP_400_BAD_REQUEST)
-
-            # エラーなし
-            try:
-                serializer.save()
-            except:
-                # データベースエラー
-                return Response({'error': 11}, status=HTTP_500_INTERNAL_SERVER_ERROR)
-
-            return Response(serializer.data, status=HTTP_201_CREATED)
-        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
